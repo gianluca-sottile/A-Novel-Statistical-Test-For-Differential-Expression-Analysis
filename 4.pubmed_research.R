e8ec46c603fe16ce00c0335efc9b6d9fedb2e2b1
Gianluca Sottile <gianluca.sottile@unipa.it>
2021-12-22 15:09:11 +0100

    Add files via upload

--- 4.pubmed_research.R
+++ 4.pubmed_research.R
@@ -3,79 +3,83 @@
 # tissue <- ifelse(TISSUE == "kirc", "\"kidney\"", "\"breast\"")
 # tumor <- "\"cancer\""
 # termini <- paste0("\"", risult.all2$Term.hy, "\"")
+universe_text <- ""
 tissue <- ifelse(TISSUE == "kirc", "kidney", "breast")
-tumor <- "cancer"
-termini <- paste0(risult.all2$Term.hy)
+tumor <- paste(tissue, "cancer")
+terms <- paste0(risult.all2$Term.hy)
+tumor_and_terms <- paste(tumor, termini, sep = " AND ")
 GO_ID <- risult.all2$GO.ID
 
-tissue_and_termini <- paste(tissue, termini, sep = " AND ")
-tumor_and_termini <- paste(tumor, termini, sep = " AND ")
-tissue_and_tumor_and_terms <- paste(tissue, tumor, termini, sep = " AND ")
+# numer of the urn
+universe <- EUtilsSummary(universe_text, type="esearch", db="pubmed", datetype='pdat', mindate=2000, maxdate=2020)
+# num_N <- QueryCount(universe)
+# number of white balls
+white_balls <- EUtilsSummary(tumor, type="esearch", db="pubmed", datetype='pdat', mindate=2000, maxdate=2020)
+# num_m <- QueryCount(white_balls)
+# number of black balls
+# num_n <- num_N - num_m
+# number of balls drawn
+# balls_drawn <- try(EUtilsSummary(terms[i], type="esearch", db="pubmed", datetype='pdat', mindate=2000, maxdate=2020), silent = TRUE)
+# while(inherits(balls_drawn, "try-error")) balls_drawn <- try(EUtilsSummary(terms[i], type="esearch", db="pubmed", datetype='pdat', mindate=2000, maxdate=2020), silent = TRUE)
+# num_k <- QueryCount(balls_drawn)
+# # number of white balls drawn
+# number_white_balls_drawn <- try(EUtilsSummary(tumor_and_terms[i], type="esearch", db="pubmed", datetype='pdat', mindate=2000, maxdate=2020), silent = TRUE)
+# while(inherits(number_white_balls_drawn, "try-error")) number_white_balls_drawn <- try(EUtilsSummary(tumor_and_terms[i], type="esearch", db="pubmed", datetype='pdat', mindate=2000, maxdate=2020), silent = TRUE)
+# num_x <- QueryCount(number_white_balls_drawn)
+# # compute p-value
+# phyper(num_x - 1, m = num_m, n = num_n, k = num_k, lower.tail = FALSE)
 
 # hypergeometric test for pubmed research of significant terms
-pvalHyper2 <- function(termini, tissue_and_termini, tumor_and_termini, tissue_and_tumor_and_terms){
-  res_N <- EUtilsSummary(termini, type="esearch", db="pubmed", datetype='pdat', mindate=2000, maxdate=2020)
-  num_N <- QueryCount(res_N)
+pvalHyper <- function(universe, tumor, terms, tumor_and_terms){
+  # numer of the urn
+  if(!inherits(universe, "EUtilsSummary")) 
+    universe <- EUtilsSummary(universe, type="esearch", db="pubmed", datetype='pdat', mindate=2000, maxdate=2020)
+  num_N <- QueryCount(universe)
+  text_N <- universe
+  res_N <- universe@querytranslation
+  # number of white balls
+  if(!inherits(tumor, "EUtilsSummary")) 
+    white_balls <- EUtilsSummary(tumor, type="esearch", db="pubmed", datetype='pdat', mindate=2000, maxdate=2020)
+  else white_balls <- tumor
+  num_m <- QueryCount(white_balls)
+  text_m <- tumor
+  res_m <- white_balls@querytranslation
+  # number of black balls
+  num_n <- num_N - num_m
+  # number of balls drawn
+  if(!inherits(terms, "EUtilsSummary")) {
+    balls_drawn <- try(EUtilsSummary(terms, type="esearch", db="pubmed", datetype='pdat', mindate=2000, maxdate=2020), silent = TRUE)
+    while(inherits(balls_drawn, "try-error")) balls_drawn <- try(EUtilsSummary(terms, type="esearch", db="pubmed", datetype='pdat', mindate=2000, maxdate=2020), silent = TRUE)
+  } else balls_drawn <- terms
+  num_k <- QueryCount(balls_drawn)
+  text_k <- terms
+  res_k <- balls_drawn@querytranslation
+  # number of white balls drawn
+  if(!inherits(tumor_and_terms, "EUtilsSummary")) {
+    number_white_balls_drawn <- try(EUtilsSummary(tumor_and_terms, type="esearch", db="pubmed", datetype='pdat', mindate=2000, maxdate=2020), silent = TRUE)
+    while(inherits(number_white_balls_drawn, "try-error")) number_white_balls_drawn <- try(EUtilsSummary(tumor_and_terms, type="esearch", db="pubmed", datetype='pdat', mindate=2000, maxdate=2020), silent = TRUE)
+  } else number_white_balls_drawn <- tumor_and_terms
+  num_x <- QueryCount(number_white_balls_drawn)
+  text_x <- tumor_and_terms
+  res_x <- number_white_balls_drawn@querytranslation
+  # calcolo del p-val
+  pval <- phyper(num_x - 1, num_m, num_n, num_k, lower.tail = FALSE)
   
-  res_k <- res_m <- res_x <- NA
-  if(num_N != 0){ 
-    # per trovare k - numero di palle estratte
-    # text_k <- tissue_and_termini[i]
-    res_k <- EUtilsSummary(tissue_and_termini, type="esearch", db="pubmed", datetype='pdat', mindate=2000, maxdate=2020)
-    num_k <- QueryCount(res_k)
-    
-    #per trovare x - intersezione
-    # text_x <- tissue_and_tumor_and_terms[i]
-    res_m <- EUtilsSummary(tumor_and_termini, type="esearch", db="pubmed", datetype='pdat', mindate=2000, maxdate=2020)
-    num_m <- QueryCount(res_m)
-    
-    ### per trovare i casi sfavorevoli fare N-m
-    num_b = num_N - num_m
-    
-    if(num_k != 0 & num_m != 0){ 
-      res_x <- EUtilsSummary(tissue_and_tumor_and_terms, type="esearch", db="pubmed", datetype='pdat', mindate=2000, maxdate=2020)
-      num_x <- QueryCount(res_x)
-      
-      # calcolo del p-val
-      pval <- phyper(num_x - 1, num_m, num_b, num_k, lower.tail = FALSE)
-    }
-    else{
-      num_x <- 0
-      pval <- 1
-    }
-  }
-  else{
-    num_k <- 0
-    num_m <- 0
-    num_x <- 0
-    num_b <- num_N
-    pval <- 1
-  }
-  
-  out <- data.frame(num_N = num_N, num_k = num_k, num_m = num_m, num_b = num_b, num_x = num_x, pval = pval)
+  out <- data.frame(num_N = num_N, num_m = num_m, num_n = num_n, num_k = num_k, num_x = num_x, pval = pval)
   attr(out, "details") <- list(
-    text_N = termini, res_N = res_N@querytranslation, 
-    text_k = tissue_and_termini, res_k = ifelse(isS4(res_k), res_k@querytranslation, res_k), 
-    text_m = tumor_and_termini, res_m = ifelse(isS4(res_m), res_m@querytranslation, res_m), 
-    text_x = tissue_and_tumor_and_terms, res_x = ifelse(isS4(res_x), res_x@querytranslation, res_x))
+    text_N = text_N, res_N = res_N, 
+    text_m = text_m, res_m = res_m, 
+    text_k = text_k, res_k = res_k, 
+    text_x = text_x, res_x = res_x)
   out
 }
 
 out2 <- NULL
-pb <- txtProgressBar(min = 0, max = length(termini), style = 3)
-for(i in 1:length(termini)){  #
+pb <- txtProgressBar(min = 0, max = length(terms), style = 3)
+for(i in 1:length(terms)){  #
   setTxtProgressBar(pb, i)
   
-  temp <- try(pvalHyper2(termini = termini[i], 
-                         tissue_and_termini = tissue_and_termini[i],
-                         tumor_and_termini = tumor_and_termini[i], 
-                         tissue_and_tumor_and_terms = tissue_and_tumor_and_terms[i]), 
-              silent = TRUE)
-  while(inherits(temp, "try-error")) temp <- try(pvalHyper2(termini = termini[i],
-                                                            tissue_and_termini = tissue_and_termini[i],
-                                                            tumor_and_termini = tumor_and_termini[i],
-                                                            tissue_and_tumor_and_terms = tissue_and_tumor_and_terms[i]),
-                                                 silent = TRUE)
+  temp <- pvalHyper(universe = universe, tumor = white_balls, terms = terms[i], tumor_and_terms = tumor_and_terms[i])
   out2 <- rbind(out2, temp)
 }
 close(pb)
@@ -130,7 +134,7 @@ venn.diagram(vpj, filename = "VennTermsBH.png",
              cat.default.pos = "outer",
              cat.fontfamily = "sans",
              cat.col = colori ,
-             cat.dist = c(0.05, 0.05, -.35),
+             cat.dist = c(0.05, 0.05, .05),
              cat.pos = 0,
              margin = 0.05 ,
              scaled = FALSE ,
@@ -158,7 +162,7 @@ venn.diagram(vpj, filename = "VennTermsBonf.png",
              cat.default.pos = "outer",
              cat.fontfamily = "sans",
              cat.col = colori ,
-             cat.dist = c(0.05, 0.05, -.2),
+             cat.dist = c(0.05, 0.05, -.45),
              cat.pos = 0,
              margin = 0.05 ,
              scaled = FALSE ,
@@ -203,7 +207,7 @@ venn.diagram(vpj, filename = "VennTermsBonf2.png",
              cat.default.pos = "outer",
              cat.fontfamily = "sans",
              cat.col = colori ,
-             cat.dist = c(0.05, 0.05, -.2),
+             cat.dist = c(0.05, 0.05, -.45),
              cat.pos = 0,
              margin = 0.05 ,
              scaled = FALSE ,
@@ -212,7 +216,7 @@ venn.diagram(vpj, filename = "VennTermsBonf2.png",
 risult.all4 <- subset(risult.all, Term %in% out3[out3$pval.bonf <= .05, "termini"])[, c("Term", "Annotated", "Significant.hy", "Significant.t", "Significant")]
 risult.all4 <- merge.data.frame(risult.all4, out3, by.x = "Term", by.y = "termini") 
 risult.all4 <- merge.data.frame(risult.all4, risult.all3[, c("Term", "rif.bonf.our", "rif.bonf.t", "rif.bonf.sam")], by.x = "Term", by.y = "Term")
-risult.all4$PVAL <- ifelse(risult.all4$pval.bonf < 1.11E-16, "1.11E-16", formatC(risult.all4$pval.bonf, digits = 2, width = 3, format = "E"))
+risult.all4$PVAL <- ifelse(risult.all4$pval.bonf < 1.11E-16, "<1.11E-16", formatC(risult.all4$pval.bonf, digits = 2, width = 3, format = "E"))
 risult.all4$group <- apply(risult.all4[,15:17], 1, function(x) ifelse(all(x == 1), "all", 
                                                                       ifelse(x[1] == 1 & x[2] == 1, "hy-test/mod t-test", 
                                                                              ifelse(x[1] == 1 & x[3] == 1, "hy-test/sam", 
